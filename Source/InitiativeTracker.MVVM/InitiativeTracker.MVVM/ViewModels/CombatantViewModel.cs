477c43d4258f3cde545e193f58de4f4e221e5acc
Alexander Parks <alec.parks@gmail.com>
2014-10-23 16:25:27 -0500

    Combatant View Model

---
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/ViewModels/CombatantViewModel.cs
@@ -0,0 +1,36 @@
+﻿using InitiativeTracker.MVVM.Models;
+
+namespace InitiativeTracker.MVVM.ViewModels
+{
+    public class CombatantViewModel
+    {
+        private readonly Combatant _combatant;
+
+        public CombatantViewModel(Combatant combatant)
+        {
+            _combatant = combatant;
+        }
+
+        //Display name
+        public string DisplayName()
+        {
+            return _combatant.DisplayName;
+        }
+
+        //Current Initiaitve
+        public string Initiative()
+        {
+            return _combatant.Initiative.Current.ToString();
+        }
+
+        //Initiative Modifier
+        public string Modifier()
+        {
+            if (_combatant.Initiative.Modifier < 0)
+                return "-" + _combatant.Initiative.Modifier;
+            if (_combatant.Initiative.Modifier > 0)
+                return "+" + _combatant.Initiative.Modifier;
+            return _combatant.Initiative.Modifier.ToString();
+        }
+    }
+}
