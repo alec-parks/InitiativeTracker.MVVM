53195097a9ffeea9d7d81d074ee06aacef8adfc8
Himrin <alec.parks@gmail.com>
2014-10-23 21:04:11 -0500

    Combat View Model start

---
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/ViewModels/CombatViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using InitiativeTracker.MVVM.Models;
+
+namespace InitiativeTracker.MVVM.ViewModels
+{
+    public class CombatViewModel
+    {
+        private readonly Combat _combat;
+
+        public CombatViewModel(Combat combat)
+        {
+            _combat = combat;
+        }
+
+        public IEnumerable<Combatant> Combatants()
+        {
+            return _combat.Combatants;
+        }
+    }
+}
