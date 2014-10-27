75d2f2733e18f73c666355722935cb27b284a2db
Alexander Parks <alec.parks@gmail.com>
2014-10-27 16:42:32 -0500

    Converter Work
    Changed converter to use brush, switch, and a default for errors.

--- Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/Views/CombatantToColorConverter.cs
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/Views/CombatantToColorConverter.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
+using System.Windows.Media;
 using InitiativeTracker.MVVM.Models;
 
 namespace InitiativeTracker.MVVM.Views
 {
     public class CombatantToColorConverter: IValueConverter
     {
+        public object Brush { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value != null)
+            CombatantType combatantType;
+
+            Enum.TryParse<CombatantType>(value.ToString(), out combatantType);
+            switch (combatantType)
             {
-                return (CombatantType)value == CombatantType.Monster 
-                    ? "#FFFF0000" 
-                    : "#FF00FF00";
+                case CombatantType.Player:
+                    Brush = "#FF00FF00";
+                    break;
+                case CombatantType.Monster:
+                    Brush = "#FFFF0000";
+                    break;
             }
-            return "#FF000000";
+
+            return Brush;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
