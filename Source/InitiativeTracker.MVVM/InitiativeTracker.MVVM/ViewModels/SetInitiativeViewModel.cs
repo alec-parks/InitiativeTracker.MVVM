16ea99e48d8bdb05d553f210b7f60a1fac1c1c56
Alexander Parks <alec.parks@gmail.com>
2014-11-04 15:53:47 -0600

    SIVM work

--- Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/ViewModels/SetInitiativeViewModel.cs
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/ViewModels/SetInitiativeViewModel.cs
@@ -1,6 +1,47 @@
-﻿namespace InitiativeTracker.MVVM.ViewModels
+﻿using System.Windows.Input;
+using Assisticant;
+using Assisticant.Fields;
+using InitiativeTracker.MVVM.Models;
+
+namespace InitiativeTracker.MVVM.ViewModels
 {
-    class SetInitiativeViewModel
+    public class SetInitiativeViewModel
     {
+        private Observable<Combatant> _combatant = new Observable<Combatant>();
+
+        public Combatant Combatant
+        {
+            get { return _combatant; }
+        }
+
+        public SetInitiativeViewModel(Combatant combatant)
+        {
+            _combatant.Value = combatant;
+        }
+
+        public string DisplayName
+        {
+            get { return _combatant.Value.DisplayName; }
+        }
+
+        public int Modifier
+        {
+            get { return _combatant.Value.Initiative.Modifier; }
+        }
+
+        public int Current
+        {
+            get { return _combatant.Value.Initiative.Current; }
+            set { _combatant.Value.Initiative.Current = value; }
+        }
+
+        public ICommand RollInitiative
+        {
+            get
+            {
+                return MakeCommand
+                    .Do(() => _combatant.Value.Initiative.Roll());
+            }
+        }
     }
 }
