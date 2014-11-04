288bbb3761f5da34771c5110487e15865f454ed9
Alexander Parks <alec.parks@gmail.com>
2014-11-03 21:04:23 -0600

    Increment Counter
    Added function to increment counter when combatant is added if another
    combatant is found with same name and same type.

--- Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/Models/Combat.cs
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM/Models/Combat.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Assisticant.Collections;
 
 namespace InitiativeTracker.MVVM.Models
@@ -14,12 +15,30 @@ namespace InitiativeTracker.MVVM.Models
 
         public void AddCombatant(Combatant combatant)
         {
+            var highCombatant = HighestCombatant(combatant);
+            if (highCombatant != null)
+            {
+                if (highCombatant.Counter == 0)
+                {
+                    highCombatant.Counter++;
+                }
+                combatant.Counter = highCombatant.Counter++;
+            }
             _combatants.Add(combatant);
         }
 
         public void RemoveCombatant(Combatant combatant)
         {
             _combatants.Remove(combatant);
+        }
+
+        private Combatant HighestCombatant(Combatant combatantToMatch)
+        {
+            var returnCombatant = _combatants
+                .Where(combatant => combatant.Name == combatantToMatch.Name)
+                .OrderByDescending(combatant => combatant.Counter)
+                .FirstOrDefault();
+            return returnCombatant;
         }
     }
 }
