8c8938a2a55bcd13130c244fe6fd71329745642f
Alexander Parks <alec.parks@gmail.com>
2015-02-09 21:30:52 -0600

    DiceRoller Tests
    Added tests to Dice Roller and made tests package a library instead of
    an empty solution.

---
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM.Tests/DiceRollerTests.cs
@@ -0,0 +1,20 @@
+﻿using InitiativeTracker.MVVM.Models;
+using Xunit;
+using Xunit.Extensions;
+
+namespace InitiativeTracker.MVVM.Tests
+{
+    public class DiceRollerTests
+    {
+        [Theory]
+        [InlineData(20, -1, 0, 19)]
+        [InlineData(20,-5,-4,15)]
+        [InlineData(20,5,6,25)]
+        public void ShouldReturnInitiative(int sides, int modifier, int min, int max)
+        {
+            var result = DiceRoller.Roll(sides, modifier);
+
+            Assert.InRange(result, min, max);
+        }
+    }
+}
