486c7e028e57f618553b11fcbdf13c2a2a8e2976
Alexander Parks <alec.parks@gmail.com>
2015-02-09 21:37:27 -0600

    Dice Roller Tests
    Changed Dice Roller Tests to reflect new functionality and API

--- Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM.Tests/DiceRollerTests.cs
+++ Source/InitiativeTracker.MVVM/InitiativeTracker.MVVM.Tests/DiceRollerTests.cs
@@ -7,12 +7,13 @@ namespace InitiativeTracker.MVVM.Tests
     public class DiceRollerTests
     {
         [Theory]
-        [InlineData(20, -1, 0, 19)]
-        [InlineData(20,-5,-4,15)]
-        [InlineData(20,5,6,25)]
-        public void ShouldReturnInitiative(int sides, int modifier, int min, int max)
+        [InlineData(1, 20, 1, 20)]
+        [InlineData(2, 10, 2, 20)]
+        [InlineData(1, 4, 1, 4)]
+        [InlineData(3, 6, 3, 18)]
+        public void ShouldReturnInitiative(int dice, int sides, int min, int max)
         {
-            var result = DiceRoller.Roll(sides, modifier);
+            var result = DiceRoller.Roll(dice, sides);
 
             Assert.InRange(result, min, max);
         }
